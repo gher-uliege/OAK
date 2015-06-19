c339d4a50b98e3f3bcb3e1f7758a1629cc7afdff
Alexander Barth <barth.alexander@gmail.com>
2015-06-19 12:28:47 +0000

    communicator

--- toymodel.F90
+++ toymodel.F90
@@ -18,15 +18,22 @@ program toymodel
  integer :: comm, nprocs, ierr, tag
  !integer :: precision = MPI_DOUBLE_PRECISION
  integer :: precision = MPI_REAL
- integer status(MPI_STATUS_SIZE)
+ integer :: status(MPI_STATUS_SIZE)
 #endif
 
  n = 100
  Ntime = n
 
 #ifdef MPI
- comm = mpi_comm_world
+
  call mpi_init(ierr)
+
+#ifdef OAK
+ call init(mpi_comm_world,comm)
+#else
+ comm = mpi_comm_world
+#endif
+
  call mpi_comm_size(comm, nprocs, ierr)
  call mpi_comm_rank(comm, rank, ierr)
 
@@ -41,6 +48,7 @@ program toymodel
 
  tag = 1
  write(6,*) 'size rank master',nprocs,rank,comm,j0,j1,k0,k1
+ call MPI_Barrier (comm, ierr )
 #else
  rank = 0
  j0 = 1
@@ -143,4 +151,58 @@ contains
 #endif
 
  end subroutine bc
+
+ 
+ subroutine init(comm,comm_ensmember)
+  implicit none
+
+  integer, intent(in) :: comm
+  integer, intent(out) :: comm_ensmember
+
+     integer NPROCS
+   parameter(NPROCS=8)
+   integer rank, new_rank, sendbuf, recvbuf, numtasks, nprocs_check
+   integer ranks1(4), ranks2(4), ierr
+   integer orig_group, new_group, new_comm
+   data ranks1 /0, 1, 2, 3/, ranks2 /4, 5, 6, 7/
+
+   call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
+   call MPI_COMM_SIZE(MPI_COMM_WORLD, nprocs_check, ierr)
+
+   if (nprocs_check .ne. NPROCS) then
+     print *, 'Must specify NPROCS= ',NPROCS,' Terminating.'
+     call MPI_FINALIZE(ierr)
+     stop
+   endif
+
+   sendbuf = rank
+
+!  Extract the original group handle
+   call MPI_COMM_GROUP(MPI_COMM_WORLD, orig_group, ierr)
+
+!  Divide tasks into two distinct groups based upon rank
+   if (rank .lt. NPROCS/2) then
+      call MPI_GROUP_INCL(orig_group, NPROCS/2, ranks1,  &
+                   new_group, ierr)
+   else 
+      call MPI_GROUP_INCL(orig_group, NPROCS/2, ranks2,  &
+                   new_group, ierr)
+   endif
+
+   call MPI_COMM_CREATE(MPI_COMM_WORLD, new_group,  &
+                    comm_ensmember, ierr)
+   call MPI_ALLREDUCE(sendbuf, recvbuf, 1, MPI_INTEGER, &
+                    MPI_SUM, comm_ensmember, ierr)
+
+   call MPI_GROUP_RANK(new_group, new_rank, ierr)
+   print *, 'rank= ',rank,' newrank= ',new_rank,' recvbuf= ', &
+       recvbuf
+
+
+   call MPI_Barrier (MPI_COMM_WORLD, ierr )
+
+
+!   stop
+ end subroutine init
+
 end program toymodel
