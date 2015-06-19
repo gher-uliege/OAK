38973713812704b65a4a08b7ba22cb7152707537
Alexander Barth <barth.alexander@gmail.com>
2015-06-19 11:35:28 +0000

    update toymodel

--- toymodel.F90
+++ toymodel.F90
@@ -5,8 +5,8 @@ program toymodel
 #endif
 
 implicit none
-integer :: n
-integer, parameter :: Ntime = 1000, halo = 1
+integer :: n, Ntime
+integer, parameter :: halo = 1
 
 real, allocatable :: x(:),xinit(:)
 integer :: i
@@ -14,10 +14,13 @@ integer :: j0, j1, k0, k1, nl
 
 #ifdef MPI
 integer :: comm, rank, nprocs, ierr, tag
-integer :: precision = MPI_DOUBLE_PRECISION
+!integer :: precision = MPI_DOUBLE_PRECISION
+integer :: precision = MPI_REAL
+integer status(MPI_STATUS_SIZE)
 #endif
 
-n = 1000
+n = 5
+Ntime = n
 
 #ifdef MPI
  comm = mpi_comm_world
@@ -26,8 +29,8 @@ n = 1000
  call mpi_comm_rank(comm, rank, ierr)
 
  ! indexes in global vector
- j0 = (rank * n)/ncpus + 1
- j1 = ((rank+1) * n)/ncpus
+ j0 = (rank * n)/nprocs + 1
+ j1 = ((rank+1) * n)/nprocs
 
  ! indexes in local vector
  nl = j1 - j0 + 1
@@ -50,9 +53,15 @@ n = 1000
 
 allocate(x(1:nl+2*halo),xinit(1:nl+2*halo))
 xinit = 0
-if (n/2 <= j1+halo) then
-  xinit(n/2 - (j0-halo)) = 1
-end if
+
+do i=k0,k1
+  xinit(i) = i-k0+j0
+end do
+
+ write(6,*) 'k0,k1,halo',k0,k1,halo
+
+call bc(i,xinit)
+write(6,*) 'xinit ',xinit, 'rank',rank
 
 x = xinit
 
@@ -65,12 +74,16 @@ do i = 1,Ntime
   call bc(i,x)
 end do
 
+write(6,*) 'x ',x, 'rank',rank
+write(6,*) 'xinit ',xinit, 'rank',rank
+
 write(6,*) 'sum(x) ',sum(x), maxval(abs(x - xinit))
 if (maxval(abs(x - xinit)) > 1e-6) then
   write(6,*) 'fail'
 else
   write(6,*) 'OK'
 end if
+
 deallocate(x,xinit)
 
 #ifdef MPI
@@ -90,8 +103,12 @@ contains
 
 
  subroutine bc(i,x)
+  implicit none
   integer, intent(in) :: i
   real, intent(inout) :: x(:)
+
+  write(6,*) 'k0,k1,halo',k0,k1,halo
+  write(6,*) 'nprocs',nprocs, rank
 
 #ifndef MPI
 
@@ -104,22 +121,33 @@ contains
 
 !  Send to the "left"
 !
- write(6,*) 'send to',modulo(rank-1,ncpus)
- call mpi_send(x(k0:k0+halo-1), halo, precision, modulo(rank-1,ncpus), tag, &
+ write(6,*) 'send to',modulo(rank-1,nprocs)
+ write(6,*) 'send to',k0,k0+halo-1
+
+
+ call mpi_send(x(k0:k0+halo-1), halo, precision, modulo(rank-1,nprocs), tag, &
         comm, ierr )
 
- call mpi_recv(x(k1+1:k1+halo), halo, precision, modulo(rank+1,ncpus), tag, &
+ write(6,*) 'get from',k1+1,k1+halo, shape(x)
+  write(6,*) 'nprocs',nprocs
+ 
+ call mpi_recv(x(k1+1:k1+halo), halo, precision, modulo(rank+1,nprocs), tag, &
         comm, status, ierr )
 
+  write(6,*) 'status, ierr',status, ierr, precision
+  write(6,*) 'nprocs',nprocs
  tag = tag+1
 
 !  Send to the "right"
 !
- write(6,*) 'send to',modulo(rank+1,ncpus)
- call mpi_send(x(k1-halo+1:k1), halo, precision, modulo(rank+1,ncpus), tag, &
+ write(6,*) 'k0,k1,halo',k0,k1,halo
+  write(6,*) 'nprocs',nprocs, rank
+ write(6,*) 'send to',modulo(rank+1,nprocs),x(k1-halo+1:k1),k0-halo,k0-1
+ call mpi_send(x(k1-halo+1:k1), halo, precision, modulo(rank+1,nprocs), tag, &
         comm, ierr )
 
- call mpi_recv(x(k0-halo:k0-1), halo, precision, modulo(rank-1,ncpus), tag, &
+ write(6,*) 'get from',modulo(rank-1,nprocs),k0-halo,k0-1
+ call mpi_recv(x(k0-halo:k0-1), halo, precision, modulo(rank-1,nprocs), tag, &
         comm, status, ierr )
 
  tag = tag+1
