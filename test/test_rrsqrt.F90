49b7b5eba9466f3b2e05c97f1ae216d64687c9d6
Alexander Barth <barth.alexander@gmail.com>
2016-06-03 13:51:27 +0000

    remove diagR subroutines

--- test/test_rrsqrt.F90
+++ test/test_rrsqrt.F90
@@ -20,99 +20,9 @@ program test_rrsqrt
    tol = 1e-8
  end if
  
- call test_analysis
  call test_analysis_covar
 
 contains
-
- subroutine test_analysis
-  use matoper
-  use rrsqrt
-  implicit none
-
-  ! number of observations
-  integer, parameter :: m = 5
-  
-  ! state vector size
-  integer, parameter :: n = 10
-  
-  ! ensemble size
-  integer, parameter :: dim_ens = 30
-
-  real :: y(m)           ! Vector of observations
-  real :: Sf(n,dim_ens)  ! 
-  real :: Ef(n,dim_ens)  ! Forecast ensemble
-  real :: xf(n)          ! Initial state (ensemble mean)
-  real :: H(m,n)         ! Observation operator
-  real :: HEf(m,dim_ens) ! Observed forecast ensemble
-  real :: HSf(m,dim_ens) ! Observed S
-  real :: Hxf(m)         ! Observed forecast
-  real :: Sa(n,dim_ens)  ! 
-  real :: xa(n)          ! Analysis state (ensemble mean)
-  real :: Efp(n,dim_ens) ! Forecast ensemble perturbations
-
-  real :: invsqrtR(m)
-  ! Observation error covariance matrix
-  real :: R(m,m)
-
-  ! variables for verification
-  real :: Pf(n,n)        ! Forecast Ensemble covariance
-  real :: K(n,m)         ! Kalman gain
-  real :: Pa_check(n,n)  ! Analysis ensemble covariance
-  real :: xa_check(n)    ! Analysis state (ensemble mean)
-    
-
-  ! index 
-  integer :: i
-
-  ! Initialize arrays
-  y = [(i,i=1,m)]
-  Ef = reshape([(sin(3.*i),i=1,n*dim_ens)],[n,dim_ens])
-  H = reshape([(i,i=1,m*n)],[m,n])
-  
-  ! R: diagonal matrix with diagonal elements equal to 2
-  R = 2*eye(m)
-  invsqrtR = 1./sqrt(2.)
-
-  ! Compute initial ensemble mean state
-  xf = sum(Ef,2)/dim_ens
-  Efp = Ef - spread(xf,2,dim_ens)
-  Sf = Efp / sqrt(dim_ens-1.)
-
-  ! Observed part
-  HEf = matmul(H,Ef)
-  HSf = matmul(H,Sf)
-  Hxf = matmul(H,xf)
-  
-  ! For verification, compute ensemble covariance and Kalman gain
-  Pf = matmul(Efp,transpose(Efp)) / (dim_ens-1)
-
-  K = matmul( &
-       matmul(Pf, transpose(H)), &
-       inv(matmul(H,matmul(Pf,transpose(H))) + R))
-  
-  ! Analyzed covariance and mean state
-  Pa_check = Pf - matmul(K,matmul(H,Pf))
-  xa_check = xf + matmul(K,(y - matmul(H,xf)))
-
-  ! Perform analysis step
-  call analysis(xf,Hxf,y,Sf,HSf,invsqrtR, xa,Sa)
-  
-  ! check results
-  call assert(xa,xa_check,tol,'analysis ensemble mean')
-
-!  write(6,*) 'xa',xa
-!  write(6,*) 'xa',xa_check
-  
-  call assert(matmul(Sa,transpose(Sa)),Pa_check, tol, &
-       'analysis ensemble variance')
-
-  
- end subroutine test_analysis
-
-
- !_______________________________________________________
- !
 
  subroutine testing_analysis_covar(xf,Sf,H,yo,CovarR)
   use covariance
@@ -227,7 +137,7 @@ contains
   Pa_check = Pf - matmul(K,matmul(H,Pf))
   xa_check = xf + matmul(K,(yo - matmul(H,xf)))
 
-  call analysis(xf,Hxf,yo,Sf,HSf, 1/sqrt(CovarR%diag()), xa,Sa)
+  call analysis_covar(xf,Hxf,yo,Sf,HSf, CovarR, xa,Sa)
 
   ! single zone
   zoneSize(1) = size(xf)
